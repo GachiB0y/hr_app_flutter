528b3099eb3ac1832630675eef50d1f66cfb3f00
alexvolkov <a.volkov@grass.su>
2024-02-20 15:35:48 +0300

    refactor Splash Screen

--- lib/features/app/logic/app_runner.dart
+++ lib/features/app/logic/app_runner.dart
@@ -34,7 +34,7 @@ final class AppRunner with InitializationSteps, InitializationProcessor {
         // Setup bloc observer and transformer
         Bloc.observer = const AppBlocObserver();
         Bloc.transformer = bloc_concurrency.sequential();
-        // await Future.delayed(const Duration(seconds: 5));
+        await Future.delayed(const Duration(seconds: 2));
 
         //Setup Firebase
         if (Platform.isIOS) {
