d97a795885a49df57ed0b6c7d55d2f2635773e70
alexvolkov <a.volkov@grass.su>
2024-03-18 11:47:53 +0300

    add All News Screen

--- lib/features/news/widget/all_news_screen.dart
+++ lib/features/news/widget/all_news_screen.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hr_app_flutter/core/router/routes.dart';
 import 'package:hr_app_flutter/features/news/bloc/caregory_bloc.dart/category_bloc.dart';
 import 'package:hr_app_flutter/features/news/bloc/event_entity_bloc/event_entity_bloc.dart';
-import 'package:hr_app_flutter/features/news/widget/create_news_screen/create_news_screen.dart';
+import 'package:hr_app_flutter/features/news/model/event_entity/new_event_entity.dart';
 import 'package:intl/intl.dart';
+import 'package:octopus/octopus.dart';
 
 /// {@template all_news_screen}
 /// AllNewsScreen widget.
@@ -45,7 +47,7 @@ class ScrollCategoriesWidget extends StatefulWidget {
 
 /// State for widget ScrollCategoriesWidget.
 class _ScrollCategoriesWidgetState extends State<ScrollCategoriesWidget> {
-  List<String> selectedCategories = [];
+  List<int> selectedCategories = [1];
 
   @override
   void initState() {
@@ -53,13 +55,19 @@ class _ScrollCategoriesWidgetState extends State<ScrollCategoriesWidget> {
     context.read<CategoryBloc>().add(const CategoryEvent.fetch());
   }
 
-  void selectItem(String item) {
+  void selectItem(int item) {
     setState(() {
-      if (selectedCategories.contains(item)) {
-        selectedCategories.remove(item);
-      } else {
-        selectedCategories.add(item);
+      if (selectedCategories.isNotEmpty) {
+        selectedCategories.removeLast();
       }
+      selectedCategories.add(item);
+
+      // if (selectedCategories.contains(item)) {
+      //   selectedCategories.remove(item);
+      // } else {
+
+      //   selectedCategories.add(item);
+      // }
     });
   }
 
@@ -89,7 +97,7 @@ class _ScrollCategoriesWidgetState extends State<ScrollCategoriesWidget> {
                               final double rightPadding =
                                   index == state.data!.length ? 25.0 : 8.0;
                               bool isSelected = selectedCategories
-                                  .contains(state.data![index].id.toString());
+                                  .contains(state.data![index].id);
                               return Padding(
                                 padding: EdgeInsets.only(
                                     left: leftPadding,
@@ -98,8 +106,12 @@ class _ScrollCategoriesWidgetState extends State<ScrollCategoriesWidget> {
                                     bottom: 10),
                                 child: GestureDetector(
                                   onTap: () {
-                                    selectItem(
-                                        state.data![index].id.toString());
+                                    selectItem(state.data![index].id);
+                                    context.read<EventEntityBloc>().add(
+                                          EventEntityEvent.update(
+                                            idTab: state.data![index].id,
+                                          ),
+                                        );
                                   },
                                   child: Container(
                                     padding: const EdgeInsets.all(12),
@@ -178,181 +190,126 @@ class _ScrollContentWithNewsState extends State<ScrollContentWithNews> {
           } else if (state is CategoryState$Processing) {
             return const Center(child: CircularProgressIndicator.adaptive());
           } else {
-            return (state.data!.filteredListEventEntity.isEmpty)
-                ? const SizedBox.shrink()
-                : Scrollbar(
-                    thumbVisibility: true,
-                    thickness: 10,
-                    radius: const Radius.circular(10),
-                    child: CustomScrollView(
-                      slivers: [
-                        SliverList(
-                          delegate: SliverChildBuilderDelegate(
-                            (BuildContext context, int index) {
-                              // return Container(
-                              //   margin: const EdgeInsets.only(
-                              //     top: 8.0,
-                              //     left: 15.0,
-                              //     right: 30.0,
-                              //   ),
-                              //   decoration: BoxDecoration(
-                              //     boxShadow: [
-                              //       BoxShadow(
-                              //         color: Colors.black.withOpacity(0.2),
-                              //         spreadRadius: 0,
-                              //         blurRadius: 8,
-                              //         offset: const Offset(0, 0),
-                              //       ),
-                              //     ],
-                              //     borderRadius: BorderRadius.circular(16),
-                              //     color:
-                              //         Theme.of(context).colorScheme.background,
-                              //   ),
-                              //   child: ListTile(
-                              //     // minLeadingWidth: 107,
-                              //     onTap: () {},
-                              //     leading: ClipRRect(
-                              //       borderRadius: BorderRadius.circular(8.0),
-                              //       child: Image.network(
-                              //         state.data!.filteredListEventEntity[index]
-                              //             .image,
-                              //         fit: BoxFit.fill,
-                              //         height: 90,
-                              //         width: 107,
-                              //       ),
-                              //     ),
-                              //     title: Column(
-                              //       crossAxisAlignment:
-                              //           CrossAxisAlignment.start,
-                              //       children: [
-                              //         _InfoDateWidget(
-                              //           text: DateFormat('dd MMMM').format(state
-                              //               .data!
-                              //               .filteredListEventEntity[index]
-                              //               .startDate),
-                              //         ),
-                              //         const SizedBox(
-                              //           height: 5,
-                              //         ),
-                              //         Text(
-                              //           state
-                              //               .data!
-                              //               .filteredListEventEntity[index]
-                              //               .title,
-                              //           style: Theme.of(context)
-                              //               .textTheme
-                              //               .titleSmall!
-                              //               .copyWith(
-                              //                   fontSize: 15,
-                              //                   fontWeight: FontWeight.w500),
-                              //         ),
-                              //       ],
-                              //     ),
-                              //     subtitle: Text(
-                              //       state.data!.filteredListEventEntity[index]
-                              //           .description,
-                              //       style: Theme.of(context)
-                              //           .textTheme
-                              //           .titleSmall!
-                              //           .copyWith(
-                              //               fontSize: 12,
-                              //               color: Theme.of(context)
-                              //                   .colorScheme
-                              //                   .outline),
-                              //       maxLines: 2,
-                              //     ),
-                              //   ),
-                              // );
-                              return GestureDetector(
-                                onTap: () {},
-                                child: Container(
-                                  // padding: const EdgeInsets.all(50),
-                                  margin: const EdgeInsets.only(
-                                    top: 8.0,
-                                    left: 15.0,
-                                    right: 30.0,
-                                  ),
-                                  decoration: BoxDecoration(
-                                    boxShadow: [
-                                      BoxShadow(
-                                        color: Colors.black.withOpacity(0.2),
-                                        spreadRadius: 0,
-                                        blurRadius: 8,
-                                        offset: const Offset(0, 0),
-                                      ),
-                                    ],
-                                    borderRadius: BorderRadius.circular(16),
-                                    color: Theme.of(context)
-                                        .colorScheme
-                                        .background,
-                                  ),
-                                  child: Row(
-                                    children: [
-                                      Padding(
-                                        padding: const EdgeInsets.only(
-                                            top: 11.0,
-                                            left: 11.0,
-                                            bottom: 9.0,
-                                            right: 22.0),
-                                        child: ClipRRect(
-                                          borderRadius:
-                                              BorderRadius.circular(8.0),
-                                          child: Image.network(
-                                            state
-                                                .data!
-                                                .filteredListEventEntity[index]
-                                                .image,
-                                            fit: BoxFit.fill,
-                                            height: 90,
-                                            width: 107,
-                                          ),
-                                        ),
-                                      ),
-                                      Column(
-                                        mainAxisSize: MainAxisSize.min,
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: [
-                                          _InfoDateWidget(
-                                            text: DateFormat('dd MMMM').format(
-                                              state
-                                                  .data!
-                                                  .filteredListEventEntity[
-                                                      index]
-                                                  .startDate,
-                                            ),
-                                          ),
-                                          Text(
-                                            state
-                                                .data!
-                                                .filteredListEventEntity[index]
-                                                .description,
-                                            style: Theme.of(context)
-                                                .textTheme
-                                                .titleSmall!
-                                                .copyWith(
-                                                    fontSize: 12,
-                                                    color: Theme.of(context)
-                                                        .colorScheme
-                                                        .outline),
-                                          ),
-                                        ],
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              );
-                            },
-                            childCount:
-                                state.data!.filteredListEventEntity.length,
-                          ),
-                        ),
-                      ],
+            if (state.data!.filteredListEventEntity.isEmpty) {
+              return const SizedBox.shrink();
+            } else {
+              final news = state.data!.filteredListEventEntity;
+              return Scrollbar(
+                thumbVisibility: true,
+                thickness: 10,
+                radius: const Radius.circular(10),
+                child: CustomScrollView(
+                  slivers: [
+                    SliverList(
+                      delegate: SliverChildBuilderDelegate(
+                        (BuildContext context, int index) {
+                          return _OneNewsElementWidget(
+                            news: news[index],
+                          );
+                        },
+                        childCount: state.data!.filteredListEventEntity.length,
+                      ),
                     ),
-                  );
+                  ],
+                ),
+              );
+            }
           }
         },
       );
+}
+
+class _OneNewsElementWidget extends StatelessWidget {
+  const _OneNewsElementWidget({
+    super.key,
+    required this.news,
+  });
+
+  final EventEntity news;
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        context.octopus.setState((state) {
+          var findSate = state..findByName('all-news');
+          findSate.add(Routes.aboutNews.node(
+            arguments: <String, String>{'id': news.id.toString()},
+          ));
+
+          return state;
+        });
+      },
+      child: Container(
+        margin: const EdgeInsets.only(
+          top: 8.0,
+          left: 15.0,
+          right: 30.0,
+        ),
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.2),
+              spreadRadius: 0,
+              blurRadius: 8,
+              offset: const Offset(0, 0),
+            ),
+          ],
+          borderRadius: BorderRadius.circular(16),
+          color: Theme.of(context).colorScheme.background,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(
+              top: 11.0, bottom: 9.0, right: 11.0, left: 11.0),
+          child: Row(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(8.0),
+                child: Image.network(
+                  news.image,
+                  fit: BoxFit.fill,
+                  height: 90,
+                  width: 107,
+                ),
+              ),
+              const SizedBox(
+                width: 22,
+              ),
+              Flexible(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    _InfoDateWidget(
+                      text: DateFormat('dd MMMM').format(
+                        news.startDate,
+                      ),
+                    ),
+                    Text(
+                      news.title,
+                      style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                            fontSize: 15,
+                            fontWeight: FontWeight.w500,
+                          ),
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    Text(
+                      news.description,
+                      style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                          fontSize: 12,
+                          color: Theme.of(context).colorScheme.outline),
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
 
 class _InfoDateWidget extends StatelessWidget {
